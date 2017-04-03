3aecc523a9b39de8962a483ec48a9b9304ae03cb
yanil3500 <yanil3500@gmail.com>
2017-04-03 11:48:23 -0700

    Adds a new controller for GitHub oauth;

---
+++ GoGoGithub/GitHubAuthController.swift
@@ -0,0 +1,27 @@
+//
+//  GitHubAuthController.swift
+//  GoGoGithub
+//
+//  Created by Elyanil Liranzo Castro on 4/3/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+class GitHubAuthController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func printTokenTapped(_ sender: Any) {
+        
+        
+    }
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
+}
--- GoGoGithub/GoGoGithub/GitHub.swift
+++ GoGoGithub/GoGoGithub/GitHub.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-let kOAuthBaseURLString = ""
+let kOAuthBaseURLString = "https://github.com/login/oauth"
 
 let gitHubClientID = "2b4ab1c78e5448906b0f"
 let gitHubClientSecret = "31c3e02b76ab95788436298fa03bdf7951a60ecb"
@@ -17,7 +17,19 @@ let gitHubClientSecret = "31c3e02b76ab95788436298fa03bdf7951a60ecb"
 class GitHub {
     static let shared = GitHub()
     
-
-    
     private init() {}
+    
+    func oAuthRequestWith(parameters: [String : String]){
+        //Parameters will equal everything after question mark in a url
+        var parametersString = ""
+        
+        for (key, value) in parameters{
+            parametersString += "&\(key)=\(value)"
+        }
+        
+        print("Inside of oAuthRequestWith method: the parameterString \(parametersString)")
+        if let requestURL = URL(string: "\(kOAuthBaseURLString)authorize?client_id=\(gitHubClientID)\(parametersString)") {
+            print("The requestURL: \(requestURL.absoluteString)")
+        }
+    }
 }
