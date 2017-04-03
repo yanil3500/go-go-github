8f754e204616486eca6852721af7d534c0930b13
yanil3500 <yanil3500@gmail.com>
2017-04-03 11:23:54 -0700

    Adds GitHub class that will serve as the go between my application and GitHub's resources.

---
+++ GoGoGithub/GoGoGithub/GitHub.swift
@@ -0,0 +1,23 @@
+//
+//  GitHub.swift
+//  GoGoGithub
+//
+//  Created by Elyanil Liranzo Castro on 4/3/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+
+let kOAuthBaseURLString = ""
+
+let gitHubClientID = "2b4ab1c78e5448906b0f"
+let gitHubClientSecret = "31c3e02b76ab95788436298fa03bdf7951a60ecb"
+
+class GitHub {
+    static let shared = GitHub()
+    
+
+    
+    private init() {}
+}
